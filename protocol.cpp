c7334660a7eb1a18a21a265c877eb038d19e4679
Frederic Rezeau <frederic.rezeau@gmail.com>
2018-10-26 19:03:52 +0900

    mag seeder parameters

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x1b, 0xee, 0x2c, 0x6e };
 
 CMessageHeader::CMessageHeader()
 {
